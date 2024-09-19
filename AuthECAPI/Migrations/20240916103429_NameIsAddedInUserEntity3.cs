bb9b0930e1e4b0bd1fd05df51f2f580cbf8c8403
Your Name <mpisanelitha@gmail.com>
2024-09-19 09:22:41 +0200

    Initial commit of ASP.Net Core Auth EC Client Web API

---
+++ AuthECAPI/Migrations/20240916103429_NameIsAddedInUserEntity3.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuthECAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class NameIsAddedInUserEntity3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FullName",
+                table: "AspNetUsers",
+                type: "nvarchar(150)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(150)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FullName",
+                table: "AspNetUsers",
+                type: "nvarchar(150)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(150)",
+                oldNullable: true);
+        }
+    }
+}
---
+++ AuthECAPI/Models/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AuthECAPI.Models
+{
+    public class AppUser:IdentityUser
+    {
+        [Column(TypeName = "nvarchar(150)")]
+        public string FullName { get; set; }
+    }
+}
